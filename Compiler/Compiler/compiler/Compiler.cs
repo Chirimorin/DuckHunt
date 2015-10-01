5d269ff80aa21652558a7810d9417ac8b9209958
JipV <jip--pie@ghotmail.com>
2015-10-01 11:02:12 +0200

    Innimini piepkleine beginnetje aan compiler

--- Compiler/Compiler/compiler/Compiler.cs
+++ Compiler/Compiler/compiler/Compiler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Compiler.action_nodes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace Compiler.compiler
 {
     public class Compiler
     {
+        private LinkedList<Node> Actions { get; set; }
+
+        public void compile()
+        {
+
+        }
+
+        public void getLastToken()
+        {
+
+        }
     }
 }
